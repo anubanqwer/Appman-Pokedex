bbabb2d99d0f61a39caadea2ff87b4da17a9a5ae
Krittapas Chaiyaprukdecha <anuban12238@hotmail.com>
2021-05-28 09:55:28 +0700

    getPokemonListAPI

---
+++ src/Function/getPokemonListAPI.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+const isPokemonType = (inputString) => {
+    const pokemonType = [
+        'Psychic',
+        'Fighting',
+        'Fairy',
+        'Normal',
+        'Grass',
+        'Metal',
+        'Water',
+        'Lightning',
+        'Darkness',
+        'Colorless',
+        'Fire'
+    ]
+
+    const inputStringLowerCase = inputString.toLowerCase();
+
+    for(let i=0; i < pokemonType.length; i++) {
+        if(inputStringLowerCase === pokemonType[i].toLowerCase()){
+            return true;
+        }
+    }
+    return false;
+
+}
+
+const getPokemonListAPI = (inputFromSeachbar) => {
+    //inputFromSearchBar is string
+
+    const queryBaseString = 'http://localhost:3030/api/cards';
+    let queryAddOn = '';
+
+    if(inputFromSeachbar != queryAddOn){
+        if (isPokemonType(inputFromSeachbar)){
+            //It's a type
+            queryAddOn = '?type=' + inputFromSeachbar;
+        }
+        else{
+            //It's name
+            queryAddOn = '?name=' + inputFromSeachbar;
+        }
+    }
+
+    axios.get(queryBaseString + queryAddOn).
+    then(res =>{
+        console.log(res.data.cards);
+        return res.data.cards;
+    }).
+    catch(err =>{
+      console.log(err);
+    });
+}
+
+export {isPokemonType, getPokemonListAPI};
--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Styles from './Modal.module.css';
 import search from './search.png';
 import PokedexCard from './ReuseComponent/PokedexCard';
+import {isPokemonType, getPokemonListAPI} from './Function/getPokemonListAPI';
 
 const Modal = ({ open, onClose }) => { 
 
+    const [queryList, setQueryList] = useState([]);
+    const [inputFromSearchBar, setInputFromSeachbar] = useState('');
+
+    const handleInputFromSearchBar = (e) => {
+        setInputFromSeachbar(e.target.value);
+    }
+
+    //------------------------------------------------- OPEN&CLOSE MODAL -------------------------------------------------
     let modalRef = useRef();
 
     useEffect(() => {
@@ -15,23 +24,29 @@ const Modal = ({ open, onClose }) => {
                     onClose();
                 }
             }
-
             document.addEventListener("mousedown", handler);
-            
+
+            //---------------------------------------------- API -----------------------------------------------------------
+            const resultFromAPI = inputFromSearchBar === '' ? getPokemonListAPI(''): getPokemonListAPI(inputFromSearchBar);
+            // const resultFromAPI = getPokemonListAPI('');
+            setQueryList(resultFromAPI);
+            // console.log(resultFromAPI); -> need promise
+            //---------------------------------------------- API -----------------------------------------------------------
 
             return () => {
                 document.removeEventListener("mousedown", handler);
             }
         }
 
-    });
+    }, [inputFromSearchBar]);
+    //------------------------------------------------- OPEN&CLOSE MODAL -------------------------------------------------
 
     if(!open) return null;
 
     return (
         <div className={Styles.overlay}>
             <div ref={modalRef} className={Styles.styleModal}>
-                <input type='text' placeholder='Find pokemon' className={Styles.searchBar}/>
+                <input type='text' placeholder='Find pokemon' onChange={handleInputFromSearchBar} className={Styles.searchBar}/>
                 <img src={search} alt='search' className={Styles.styleSearchImage} />
                 <div className={Styles.cardAreaInModal}>
                     <PokedexCard typeOfCard='long'
