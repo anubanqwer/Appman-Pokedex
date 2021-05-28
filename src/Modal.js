3110ecdf288387a39585569dba9f4bb03047b5f8
Krittapas Chaiyaprukdecha <anuban12238@hotmail.com>
2021-05-28 10:49:53 +0700

    close modal with react-outside-click-handler

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 import Styles from './Modal.module.css';
 import search from './search.png';
 import PokedexCard from './ReuseComponent/PokedexCard';
@@ -13,79 +14,66 @@ const Modal = ({ open, onClose }) => {
         setInputFromSeachbar(e.target.value);
     }
 
-    //------------------------------------------------- OPEN&CLOSE MODAL -------------------------------------------------
-    let modalRef = useRef();
-
     useEffect(() => {
-
-        if(open){
-            let handler = (event) => {
-                if(!modalRef.current.contains(event.target)) {
-                    onClose();
-                }
-            }
-            document.addEventListener("mousedown", handler);
-
-            //---------------------------------------------- API -----------------------------------------------------------
-            const resultFromAPI = inputFromSearchBar === '' ? getPokemonListAPI(''): getPokemonListAPI(inputFromSearchBar);
-            // const resultFromAPI = getPokemonListAPI('');
-            setQueryList(resultFromAPI);
-            // console.log(resultFromAPI); -> need promise
-            //---------------------------------------------- API -----------------------------------------------------------
-
-            return () => {
-                document.removeEventListener("mousedown", handler);
-            }
-        }
+        //---------------------------------------------- API -----------------------------------------------------------
+        const resultFromAPI = inputFromSearchBar === '' ? getPokemonListAPI(''): getPokemonListAPI(inputFromSearchBar);
+        setQueryList(resultFromAPI);
+        // console.log(resultFromAPI); -> need promise
+        //---------------------------------------------- API -----------------------------------------------------------
 
     }, [inputFromSearchBar]);
-    //------------------------------------------------- OPEN&CLOSE MODAL -------------------------------------------------
 
     if(!open) return null;
 
     return (
         <div className={Styles.overlay}>
-            <div ref={modalRef} className={Styles.styleModal}>
-                <input type='text' placeholder='Find pokemon' onChange={handleInputFromSearchBar} className={Styles.searchBar}/>
-                <img src={search} alt='search' className={Styles.styleSearchImage} />
-                <div className={Styles.cardAreaInModal}>
-                    <PokedexCard typeOfCard='long'
-                    imageUrl='https://images.pokemontcg.io/ex14/85.png'
-                    pokemonName='Cutto'
-                    hpPercent={90}
-                    strPercent={20}
-                    weakPercent={50}
-                    numberOfHappiness={5}/>
-                    <PokedexCard typeOfCard='long'
-                    imageUrl='https://images.pokemontcg.io/ex14/85.png'
-                    pokemonName='Cutto'
-                    hpPercent={90}
-                    strPercent={20}
-                    weakPercent={50}
-                    numberOfHappiness={5}/> 
-                    <PokedexCard typeOfCard='long'
-                    imageUrl='https://images.pokemontcg.io/ex14/85.png'
-                    pokemonName='Cutto'
-                    hpPercent={90}
-                    strPercent={20}
-                    weakPercent={50}
-                    numberOfHappiness={5}/> 
-                    <PokedexCard typeOfCard='long'
-                    imageUrl='https://images.pokemontcg.io/ex14/85.png'
-                    pokemonName='Cutto'
-                    hpPercent={90}
-                    strPercent={20}
-                    weakPercent={50}
-                    numberOfHappiness={5}/> 
-                    <PokedexCard typeOfCard='long'
-                    imageUrl='https://images.pokemontcg.io/ex14/85.png'
-                    pokemonName='Cutto'
-                    hpPercent={90}
-                    strPercent={20}
-                    weakPercent={50}
-                    numberOfHappiness={5}/>                     
+            <OutsideClickHandler
+            onOutsideClick={() => {
+                onClose();
+            }}
+            >
+                <div className={Styles.styleModal}>
+                    <input type='text' placeholder='Find pokemon' onChange={handleInputFromSearchBar} className={Styles.searchBar}/>
+                    <img src={search} alt='search' className={Styles.styleSearchImage} />
+                    <div className={Styles.cardAreaInModal}>
+                        <PokedexCard typeOfCard='long'
+                        imageUrl='https://images.pokemontcg.io/ex14/85.png'
+                        pokemonName='Cutto'
+                        hpPercent={90}
+                        strPercent={20}
+                        weakPercent={50}
+                        numberOfHappiness={5}/>
+                        <PokedexCard typeOfCard='long'
+                        imageUrl='https://images.pokemontcg.io/ex14/85.png'
+                        pokemonName='Cutto'
+                        hpPercent={90}
+                        strPercent={20}
+                        weakPercent={50}
+                        numberOfHappiness={5}/> 
+                        <PokedexCard typeOfCard='long'
+                        imageUrl='https://images.pokemontcg.io/ex14/85.png'
+                        pokemonName='Cutto'
+                        hpPercent={90}
+                        strPercent={20}
+                        weakPercent={50}
+                        numberOfHappiness={5}/> 
+                        <PokedexCard typeOfCard='long'
+                        imageUrl='https://images.pokemontcg.io/ex14/85.png'
+                        pokemonName='Cutto'
+                        hpPercent={90}
+                        strPercent={20}
+                        weakPercent={50}
+                        numberOfHappiness={5}/> 
+                        <PokedexCard typeOfCard='long'
+                        imageUrl='https://images.pokemontcg.io/ex14/85.png'
+                        pokemonName='Cutto'
+                        hpPercent={90}
+                        strPercent={20}
+                        weakPercent={50}
+                        numberOfHappiness={5}/>                     
+                    </div>
                 </div>
-            </div>
+            </OutsideClickHandler>
         </div>
     );
 }
